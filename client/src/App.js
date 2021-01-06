56fe07a15d3d72e7eed1736490f267e57cf47a25
Nathan Kane <kaneitalia.rana@gmail.com>
2021-01-05 23:07:31 -0700

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from "./demo/Home";
+import Home from "./components/Home";
 import Things from './demo/Things';
 import NoMatch from './components/NoMatch';
 import { Route, Switch } from 'react-router-dom';
